e4d77326b76dd0d9b3f1bf33ae967aa57d6c05e8
Kyle Kelley <kylekelley711@gmail.com>
2024-05-26 10:41:05 -0600

    counter btn width

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -13,8 +13,8 @@ const Counter = () => {
         <div className='mx-auto text-center'>
             <h1>{count}</h1>
             <div className='grid grid-rows-3 gap-4 mt-10'>
-                <button className='text-black hover:text-white p-2 bg-blue-400 hover:bg-blue-600 w-1/4 rounded-xl mx-auto row-span-1' onClick={() => setCount(count + 1)}>click me</button>
-                <button className='text-black hover:text-white p-2 bg-blue-400 hover:bg-blue-600 w-1/4 rounded-xl mx-auto row-span-1' onClick={() => setCount(count - 1)}>no click me</button>
+                <button className='text-black hover:text-white p-2 bg-blue-400 hover:bg-blue-600 w-3/4 md:w-1/4 rounded-xl mx-auto row-span-1' onClick={() => setCount(count + 1)}>click me</button>
+                <button className='text-black hover:text-white p-2 bg-blue-400 hover:bg-blue-600 w-3/4 md:w-1/4 rounded-xl mx-auto row-span-1' onClick={() => setCount(count - 1)}>no click me</button>
             </div>
         </div>
     )
