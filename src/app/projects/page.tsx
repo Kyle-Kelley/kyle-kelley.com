98e04972d83a65453141ae19159445367f25f8aa
Kyle Kelley <kylekelley711@gmail.com>
2024-05-28 21:21:51 -0600

    changing file names

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link  from 'next/link'; 
-import Counter from '../components/Counter';
+import Counter from '../components/CounterNew';
 import ToDoApp from '../components/ToDoApp';
 
 interface Page {
